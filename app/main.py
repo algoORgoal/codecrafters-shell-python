05f7890ab5032df96556dc16b2d92fe3773b9794
mac <mac@Byeolchans-MacBook-Pro.local>
2026-05-22 17:05:48 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,13 +4,31 @@ import sys
 def main():
     sys.stdout.write("$ ")
     command = input()
+    command_name = command.split()[0]
 
-    if command == "exit":
+    if command_name == "exit":
         return
-
-    print(f"{command}: command not found")
+    elif command_name == "echo":
+        echo(command)
+    else:
+        print(f"{command}: command not found")
 
     main()
+
+
+def echo(text):
+    command_name = "echo"
+    if text.startswith(command_name):
+        echo(text[len(command_name):])
+        return
+
+    space = " "
+
+    if text.startswith(space):
+        echo(text[len(space):])
+        return
+
+    print(text)
 
 
 if __name__ == "__main__":
