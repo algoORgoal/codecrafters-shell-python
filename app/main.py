a2d50f28487098ead6f9cc292861a1899e271b79
mac <mac@Byeolchans-MacBook-Pro.local>
2026-05-22 16:43:22 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,10 +4,13 @@ import sys
 def main():
     sys.stdout.write("$ ")
     command = input()
-    print(f"{command}: command not found")
-    main()
 
-    pass
+    if command == "exit":
+        return
+
+    print(f"{command}: command not found")
+
+    main()
 
 
 if __name__ == "__main__":
